b9f6064ff2c8281f9334985e8df5947e34ee7629
Francis Bourgouin <netbeuix@gmail.com>
2019-10-23 15:07:26 -0400

    added followcode

---
+++ follow_code.js
@@ -0,0 +1,48 @@
+// On REPL at https://repl.it/@FrancisBourgoui/jsWorkshopFollowSheet?language=nodejs, they open a copy to follow along the instructions
+
+// Initial Content, we will use this for this workshop!
+
+// 1 oak tree that measures 10.6 meters, 45 years old. The tree looks like 🌳 and is indigenous to Canada.
+
+// 1 pine tree that measures 12.6 meters, 65 years old. The tree looks like 🌲 and is indigenous to Canada.
+
+// 1 palm tree that measures 5.6 meters, 25 years old. The tree looks like 🌴 and is not indigenous to Canada.
+ 
+
+// Try console.log:
+
+// Write the sentences as strings:
+
+// Store them in variables & output the result:
+
+// Create the variables for each changing element:
+
+// Try to concatenate two string variables:
+
+// Change some variable names:
+
+// Create your first object based on the previous variables:
+
+// Access information from the object using the dot notation:
+
+// Access information from the object using the square bracket notation:
+
+// Create another object for another tree:
+
+// Create your first array containing some random elements:
+
+// Store your objects in an array:
+
+// Test a few comparisons:
+
+// Write an if statement assigning is or is not:
+
+// Write a function wrapping the previous if statement:
+
+// Write a basic for loop:
+
+// Loop the operations to show a tree statement:
+
+// Write a function containing the loop, accepting a tree array as parameter:
+
+// Congrats!
